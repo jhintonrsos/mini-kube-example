029fbe9e1c0574622bc9ce71946cbfbe011bf5d3
John Hinton <jhinton@rapidsos.com>
2019-11-30 21:40:13 -0500

    update

--- deploy.sh
+++ deploy.sh
@@ -10,7 +10,7 @@ docker build . -t ${app_name}:latest && \
 
 kubectl run ${app_name} --image=${app_name}:latest --port=8080 --image-pull-policy=Never && \
 
-kubectl expose deployment ${app_name} --type="LoadBalancer"
+kubectl expose deployment ${app_name} --type=LoadBalancer
 
 minikube service ${app_name} --url
 
